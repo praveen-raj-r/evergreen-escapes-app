ad6a46e33981c38c59b34d680675ceba2479e9f5
praveenraj <105029450+praveen-raj-r@users.noreply.github.com>
2024-09-16 17:17:09 +0530

    updated protected route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import GlobalStyles from "./styles/GlobalStyles";
 import Dashboard from "./pages/Dashboard";
@@ -12,14 +10,16 @@ import Account from "./pages/Account";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
+import Booking from "./pages/Booking";
+import Checkin from "./pages/Checkin";
+import ProtectedRoute from "./ui/ProtectedRoute";
 
 const queryClient = new QueryClient({
   defaultOptions: {
-    queries: {
-      // staleTime: 60 * 1000,
-      staleTime: 0,
-    },
+    queries: { staleTime: 0 },
   },
 });
 
@@ -27,14 +27,21 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
-
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route element={<AppLayout />}>
+          <Route
+            element={
+              <ProtectedRoute>
+                <AppLayout />
+              </ProtectedRoute>
+            }
+          >
             <Route index element={<Navigate replace to="dashboard" />} />
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="bookings" element={<Bookings />} />
+            <Route path="bookings/:bookingId" element={<Booking />} />
+            <Route path="checkin/:bookingId" element={<Checkin />} />
             <Route path="cabins" element={<Cabins />} />
             <Route path="users" element={<Users />} />
             <Route path="settings" element={<Settings />} />
@@ -45,15 +52,12 @@ function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-
       <Toaster
         position="top-center"
         gutter={12}
         containerStyle={{ margin: "8px" }}
         toastOptions={{
-          success: {
-            duration: 3000,
-          },
+          success: { duration: 3000 },
           error: {
             duration: 5000,
           },
--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -8,17 +8,17 @@ const FullPage = styled.div`
   height: 100vh;
   background-color: var(--color-grey-50);
   display: flex;
-  justify-content: center;
   align-items: center;
+  justify-content: center;
 `;
 
-function ProjectedRoute({ children }) {
+function ProtectedRoute({ children }) {
   const navigate = useNavigate();
 
-  // 1.load the authenticated user
+  // 1. Load the authenticated user
   const { isLoading, isAuthenticated } = useUser();
 
-  // 2. if there is No authenticated user, redirect to the /login
+  // 2. If there is NO authenticated user, redirect to the /login
   useEffect(
     function () {
       if (!isAuthenticated && !isLoading) navigate("/login");
@@ -26,7 +26,7 @@ function ProjectedRoute({ children }) {
     [isAuthenticated, isLoading, navigate]
   );
 
-  // 3. while loading, show a spinner
+  // 3. While loading, show a spinner
   if (isLoading)
     return (
       <FullPage>
@@ -34,9 +34,8 @@ function ProjectedRoute({ children }) {
       </FullPage>
     );
 
-  // 4. if there is a user, render the app
-
+  // 4. If there IS a user, render the app
   if (isAuthenticated) return children;
 }
 
-export default ProjectedRoute;
+export default ProtectedRoute;
