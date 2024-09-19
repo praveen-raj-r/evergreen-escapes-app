56fb5860a63dfe52dcb45fd271223918de01004d
praveenraj <105029450+praveen-raj-r@users.noreply.github.com>
2024-09-19 12:21:45 +0530

    updated ui files

--- src/ui/Menus.jsx
+++ src/ui/Menus.jsx
@@ -87,6 +87,7 @@ function Toggle({ id }) {
   const { openId, close, open, setPosition } = useContext(MenusContext);
 
   function handleClick(e) {
+    e.stopPropagation();
     const { x, y, width, height } = e.target
       .closest("button")
       .getBoundingClientRect();
@@ -105,7 +106,7 @@ function Toggle({ id }) {
 
 function List({ id, children }) {
   const { openId, position, close } = useContext(MenusContext);
-  const ref = useOutsideClick(close);
+  const ref = useOutsideClick(close, false);
   if (openId !== id) return null;
   return createPortal(
     <StyledList ref={ref} position={position}>
--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -8,17 +8,17 @@ const FullPage = styled.div`
   height: 100vh;
   background-color: var(--color-grey-50);
   display: flex;
-  align-items: center;
   justify-content: center;
+  align-items: center;
 `;
 
 function ProtectedRoute({ children }) {
   const navigate = useNavigate();
 
-  // 1. Load the authenticated user
+  // 1.load the authenticated user
   const { isLoading, isAuthenticated } = useUser();
 
-  // 2. If there is NO authenticated user, redirect to the /login
+  // 2. if there is No authenticated user, redirect to the /login
   useEffect(
     function () {
       if (!isAuthenticated && !isLoading) navigate("/login");
@@ -26,7 +26,7 @@ function ProtectedRoute({ children }) {
     [isAuthenticated, isLoading, navigate]
   );
 
-  // 3. While loading, show a spinner
+  // 3. while loading, show a spinner
   if (isLoading)
     return (
       <FullPage>
@@ -34,7 +34,8 @@ function ProtectedRoute({ children }) {
       </FullPage>
     );
 
-  // 4. If there IS a user, render the app
+  // 4. if there is a user, render the app
+
   if (isAuthenticated) return children;
 }
 
