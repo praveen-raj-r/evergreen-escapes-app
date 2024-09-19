8b02f865292002c50cf8b975000b4e498b6eae52
praveenraj <105029450+praveen-raj-r@users.noreply.github.com>
2024-09-19 12:20:05 +0530

    UPDATE

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,9 +14,10 @@ module.exports = {
   rules: {
     "no-unused-vars": "warn",
     "react/prop-types": "off",
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
+    "react-refresh/only-export-components": "off",
+    // "react-refresh/only-export-components": [
+    //   "warn",
+    //   { allowConstantExport: true },
+    // ],
   },
 };
