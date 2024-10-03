bd1e4570bd62b205744f5ef260480214a6243c56
praveenraj <105029450+praveen-raj-r@users.noreply.github.com>
2024-10-03 12:56:19 +0530

    Update GlobalStyles.js

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,5 +1,23 @@
 import { createGlobalStyle } from "styled-components";
 
+const size = {
+  xs: "400px", // for small screen mobile
+  sm: "600px", // for mobile screen
+  md: "900px", // for tablets
+  lg: "1280px", // for laptops
+  xl: "1440px", // for desktop / monitors
+  xxl: "1920px", // for big screens
+};
+
+export const device = {
+  xs: `(max-width: ${size.xs})`,
+  sm: `(max-width: ${size.sm})`,
+  md: `(max-width: ${size.md})`,
+  lg: `(max-width: ${size.lg})`,
+  xl: `(max-width: ${size.xl})`,
+  xxl: `(max-width: ${size.xxl})`,
+};
+
 const GlobalStyles = createGlobalStyle`:root {
     &,
   &.light-mode {
@@ -222,4 +240,5 @@ const GlobalStyles = createGlobalStyle`:root {
   --image-opacity: 90%;
   */
   `;
+
 export default GlobalStyles;
