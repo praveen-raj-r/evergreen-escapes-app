2d664ef85743064d3ad0e38a3f8a968bc89135bc
praveenraj <105029450+praveen-raj-r@users.noreply.github.com>
2024-10-03 15:27:01 +0530

    Update Header.jsx

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -2,21 +2,36 @@ import styled from "styled-components";
 import HeaderMenu from "./HeaderMenu";
 import UserAvatar from "../features/authentication/UserAvatar";
 
+import ButtonIcon from "./ButtonIcon";
+import { BsCaretLeftSquare } from "react-icons/bs";
+
 const StyledHeader = styled.header`
   background-color: var(--color-grey-0);
-  padding: 1.2rem 4.8rem;
+  padding: 1.2rem 1.8rem;
   border-bottom: 1px solid var(--color-grey-100);
 
   display: flex;
   gap: 2.4rem;
   align-items: center;
-  justify-content: flex-end;
+  justify-content: space-between;
 `;
+
+const StyledFlexContainer = styled.div`
+  display: flex;
+  gap: 2.4rem;
+  align-items: center;
+`;
+
 function Header() {
   return (
     <StyledHeader>
-      <UserAvatar />
-      <HeaderMenu />
+      <ButtonIcon>
+        <BsCaretLeftSquare />
+      </ButtonIcon>
+      <StyledFlexContainer>
+        <UserAvatar />
+        <HeaderMenu />
+      </StyledFlexContainer>
     </StyledHeader>
   );
 }
