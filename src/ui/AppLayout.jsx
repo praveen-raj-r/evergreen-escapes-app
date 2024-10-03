4519dc39528e8c3d99572f5142837ddec73cfb58
praveenraj <105029450+praveen-raj-r@users.noreply.github.com>
2024-10-03 15:28:00 +0530

    Update AppLayout.jsx

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -2,11 +2,16 @@ import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import styled from "styled-components";
+import { device } from "../styles/GlobalStyles";
 const StyledAppLayout = styled.div`
   display: grid;
   grid-template-columns: 26rem 1fr;
   grid-template-rows: auto 1fr;
   height: 100vh;
+
+  @media ${device.sm} {
+    
+  }
 `;
 
 const Main = styled.main`
@@ -14,6 +19,7 @@ const Main = styled.main`
   padding: 4rem 4.8rem 6.4rem;
   overflow: scroll;
 `;
+
 const Container = styled.div`
   max-width: 120rem;
   margin: 0 auto;
@@ -21,6 +27,7 @@ const Container = styled.div`
   flex-direction: column;
   gap: 3.2rem;
 `;
+
 function AppLayout() {
   return (
     <StyledAppLayout>
