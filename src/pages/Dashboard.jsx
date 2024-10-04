7dc9cd0d725f30e95e8e48ccaba1752618b1df45
praveenraj <105029450+praveen-raj-r@users.noreply.github.com>
2024-10-04 14:01:57 +0530

    alignment

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,15 +1,28 @@
+import styled from "styled-components";
 import DashboardFilter from "../features/dashboard/DashboardFilter";
 import DashboardLayout from "../features/dashboard/DashboardLayout";
 import Heading from "../ui/Heading";
-import Row from "../ui/Row";
+import { device } from "../styles/GlobalStyles";
+
+const StyledRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  @media ${device.md} {
+    flex-direction: column;
+    gap: 10px;
+    align-items: flex-start;
+  }
+`;
 
 function Dashboard() {
   return (
     <>
-      <Row type="horizontal">
+      <StyledRow type="horizontal">
         <Heading as="h1">Dashboard</Heading>
         <DashboardFilter />
-      </Row>
+      </StyledRow>
 
       <DashboardLayout />
     </>
