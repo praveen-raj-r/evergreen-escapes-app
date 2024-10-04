e2f3552ac3cb765115b74c5e8fb1d5d256cb5d7c
praveenraj <105029450+praveen-raj-r@users.noreply.github.com>
2024-10-04 14:05:35 +0530

    alignment layout

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -17,7 +17,7 @@ const StyledAppLayout = styled.div`
 
 const Main = styled.main`
   background-color: var(--color-grey-50);
-  padding: 4rem 4.8rem 6.4rem;
+  padding: 4rem 2.8rem;
 `;
 
 const Container = styled.div`
